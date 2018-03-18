13561ded257251582123a52c1f319720dc4209a1
Mikko Leinonen <mikko.leinonen91@gmail.com>
2018-03-18 19:11:30 +0200

    testit tehty Unicafe sovellukseen

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -10,11 +10,68 @@ public class MaksukorttiTest {
 
     @Before
     public void setUp() {
-        kortti = new Maksukortti(10);
+        kortti = new Maksukortti(1000);
     }
 
     @Test
     public void luotuKorttiOlemassa() {
         assertTrue(kortti!=null);      
     }
+    
+    @Test
+    public void rahatRiittivätMaukkaaseen() {
+        assertTrue(kortti.otaRahaa(400) == true);
+    }
+    
+    @Test
+    public void rahatRiittivätEdulliseen() {
+        assertTrue(kortti.otaRahaa(240) == true);
+    }
+    
+    @Test
+    public void rahatEivätRiitäPalauttaaFalse() {
+        kortti.otaRahaa(400);
+        kortti.otaRahaa(400);
+        assertTrue(kortti.otaRahaa(240) == false);
+    }
+    
+    @Test
+    public void konstruktoriAsettaaSaldonOikein() {
+        assertEquals("saldo: 10.0", kortti.toString());
+    }
+    
+    @Test
+    public void kortilleVoiLadataRahaa() {
+        kortti.lataaRahaa(1500);
+        assertEquals("saldo: 25.0", kortti.toString());
+    }
+    
+    @Test
+    public void syoEdullisestiVahentaaSaldoaOikein() {
+        kortti.otaRahaa(240);
+        assertEquals("saldo: 7.60", kortti.toString());
+    }
+    
+    @Test
+    public void syoMaukkaastiVahentaaSaldoaOikein() {
+        kortti.otaRahaa(400);
+        assertEquals("saldo: 6.0", kortti.toString());
+    }
+    
+    @Test
+    public void syoEdullisestiEiVieSaldoaNegatiiviseksi() {
+        kortti.otaRahaa(400);
+        kortti.otaRahaa(400);
+        kortti.otaRahaa(240);
+        assertEquals("saldo: 2.0", kortti.toString());
+    }
+    
+    @Test
+    public void syoMaukkaastiEiVieSaldoaNegatiiviseksi() {
+        kortti.otaRahaa(400);
+        kortti.otaRahaa(400);
+        kortti.otaRahaa(400);
+        assertEquals("saldo: 2.0", kortti.toString());
+    }
+    
 }
