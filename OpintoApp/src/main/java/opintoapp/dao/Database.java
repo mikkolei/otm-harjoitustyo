7cca29c3c4386cbca74823d3d686dc63f38e837e
Mikko Leinonen <mikko.leinonen91@gmail.com>
2018-04-24 18:41:38 +0300

    database modified

--- OpintoApp/src/main/java/opintoapp/dao/Database.java
+++ OpintoApp/src/main/java/opintoapp/dao/Database.java
@@ -1,9 +1,7 @@
 
 package opintoapp.dao;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class Database {
     
@@ -11,6 +9,28 @@ public class Database {
 
     public Database(String databaseAddress) throws ClassNotFoundException {
         this.databaseAddress = databaseAddress;
+        try {
+            Connection conn = DriverManager.getConnection(databaseAddress);
+            Statement stmt = conn.createStatement();
+            stmt.execute("CREATE TABLE IF NOT EXISTS User"
+                    + "(id integer PRIMARY KEY,"
+                    + "name varchar(50),"
+                    + "username varchar(50),"
+                    + "password varchar(50)"
+                    + ");");
+            
+            stmt.execute("CREATE TABLE IF NOT EXISTS Course"
+                    + "(id integer PRIMARY KEY,"
+                    + "user_id integer,"
+                    + "name varchar(100),"
+                    + "credits integer,"
+                    + "done boolean,"
+                    + "grade integer,"
+                    + "FOREIGN KEY (user_id) REFERENCES User(id)"
+                    + ");");
+        } catch (Exception e) {
+            
+        }
     }
     
     public Connection getConnection() throws SQLException {
